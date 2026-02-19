662bcdb9c5e8e444755fe430b97edd9eae6bc063
Hung Nguyen <nthung2112@gmail.com>
2026-02-19 12:47:41 +0700

    Initial commit

---
+++ app/(dashboard)/dashboard/setting/page.tsx
@@ -0,0 +1,15 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
+export default async function Page() {
+	return (
+		<>
+			<div className="px-4 lg:px-6">
+				<h1 className="text-lg font-medium">Appearance</h1>
+				<p className="text-sm text-muted-foreground mb-2">
+					Choose your preferred appearance settings.
+				</p>
+				<ModeToggle />
+			</div>
+		</>
+	);
+}
---
+++ proxy.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+
+export async function proxy(request: NextRequest) {
+	const sessionCookie = getSessionCookie(request);
+
+	const { pathname } = request.nextUrl;
+
+	// Redirect authenticated users away from login/signup pages
+	if (sessionCookie && ["/login", "/signup"].includes(pathname)) {
+		return NextResponse.redirect(new URL("/dashboard", request.url));
+	}
+
+	// Redirect unauthenticated users trying to access protected routes
+	if (!sessionCookie && pathname.startsWith("/dashboard")) {
+		return NextResponse.redirect(new URL("/signup", request.url));
+	}
+
+	return NextResponse.next();
+}
+
+export const config = {
+	// Apply middleware to these routes
+	matcher: ["/dashboard", "/login", "/signup"],
+};
